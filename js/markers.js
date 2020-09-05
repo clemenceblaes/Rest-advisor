d31378b87e121a6351caae0b1783613bab36542d
BLAES Clémence <clemence.blaes@laposte.net>
2020-09-05 15:51:12 +0200

    Add markers.js via Upload
    This is the file that contains script used for markers on map.

---
+++ js/markers.js
@@ -0,0 +1,25 @@
+"use strict";
+
+class Markers {
+
+  static displayMarkers() {
+
+    $.getJSON("../json/restaurants.json", (json) => {
+
+        var icon = {
+            url: "http://maps.google.com/mapfiles/ms/icons/orange-dot.png",
+            scaledSize: new google.maps.Size(50, 50),
+        };
+
+        json.existingRestaurants.forEach(restaurant => {
+            let restPosition = new google.maps.LatLng(restaurant.lat, restaurant.long);
+            let restMarker = new google.maps.Marker({
+            icon: icon,
+            position: restPosition,
+            map: map,
+            title: restaurant.restaurantName
+            })
+        })
+    })
+}
+}
