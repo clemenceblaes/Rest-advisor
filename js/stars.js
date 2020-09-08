f7e6798b09b1d77f56b945e646ca5eb8577ded72
BLAES Clémence <clemence.blaes@laposte.net>
2020-09-08 22:23:50 +0200

    Add stars.js via Upload
    This is the file that contains all scripts about star's options. (Filter, Display, etc...)

---
+++ js/stars.js
@@ -0,0 +1,71 @@
+class Stars {
+
+    static displayStars() {
+
+        $.getJSON("../json/restaurants.json", (json) => {
+   
+            for (let idRestaurant = 0; idRestaurant < json.existingRestaurants.length; idRestaurant++) {
+                let restaurantStars = [];
+                for (let existingStars = 0; existingStars < json.existingRestaurants[idRestaurant].ratings.length; existingStars++) {
+                    restaurantStars.push(parseInt(json.existingRestaurants[idRestaurant].ratings[existingStars].stars));
+
+                }
+                 //This is the calculation to obtain average notation for each restaurant.
+                const reducer = (accumulator, currentValue) => accumulator + currentValue;
+                let restaurantNumberStar = restaurantStars.reduce(reducer);   
+                let restaurantAverageStars = Math.round((restaurantNumberStar / json.existingRestaurants[idRestaurant].ratings.length)*100)/100; 
+
+
+                //Update average information on the restaurant's table.
+                let thisRestaurantName = json.existingRestaurants[idRestaurant].restaurantName;
+                let restaurantWithStar = Store.restaurants.find(element => element.name === thisRestaurantName);
+                restaurantWithStar.average = restaurantAverageStars;
+
+                //Display one or a half star in terms of restaurant's average notatation.
+                const displayAverageStars = "<div class=\"star\">" +  "</div>";
+                const displayHalfStar = "<div class=\"halfStar\">" +  "</div>";
+                for(let numberStars = 1; numberStars <= restaurantAverageStars; numberStars++) {
+                    $("#restStar" + idRestaurant).append(displayAverageStars);
+                }
+
+                //If we obtain a float number, we display a half star
+                if (Number.isInteger(restaurantAverageStars) == false) { 
+                    $("#restStar" + idRestaurant).append(displayHalfStar);
+                }
+                $("#restStar" + idRestaurant).css("display", "flex");
+            }
+        });
+    }
+
+    static filterByStars() {
+
+        $(document).ready(function() {
+            $(".filterStar").click(function() {
+                let filterChoice = $(this).attr("id");
+                    
+                $(".restaurantName").css("display", "none");
+                let filterRestaurant = Store.restaurants.filter(element => {
+                    console.log(element.average, element.average >= Number(filterChoice), element.average < Number(filterChoice) + 1);
+                    return element.average >= Number(filterChoice) && element.average < Number(filterChoice) + 1;
+                })
+
+                filterRestaurant.forEach(element => {
+                    console.log("#" + element.id);
+                    $("#" + element.id).css("display", "block");           
+                }) 
+                Store.actualisationRestaurants = filterRestaurant;
+                console.log(Store.actualisationRestaurants);
+                Restaurants.displayMarkers();
+            })
+        });
+
+        $(document).ready(function() {
+            $(".cancelFilter").click(function() {
+                $(".restaurantName").css("display", "block");
+                Store.actualisationRestaurants = Store.restaurants;
+                console.log(Store.actualisationRestaurants);
+                Restaurants.displayMarkers();
+            })
+        });
+    }
+}
