1358a9b85732b1b914de6c8698b93fb71c95feb4
BLAES Clémence <clemence.blaes@laposte.net>
2020-10-21 14:05:51 +0200

    Add store.js
    This file contains static variables used in the project.

---
+++ js/store.js
@@ -0,0 +1,9 @@
+"use strict";
+
+export default class Store {
+    static pos;
+    static restaurants = [];
+    static restaurantPlaceID;
+    static actualisationRestaurants;
+    static markersArray = [];
+}
