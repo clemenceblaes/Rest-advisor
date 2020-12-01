30db7baaa4cb8e76e935c92ad54e102f1f3c9195
BLAES Clémence <clemence.blaes@laposte.net>
2020-12-01 18:04:05 +0100

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,9 +1,9 @@
 "use strict";
 
-export default class Store {
-    static pos;
-    static restaurants = [];
-    static restaurantPlaceID;
-    static actualisationRestaurants;
-    static markersArray = [];
+export default class Store { // Class that store all global variables.
+    static pos; //The GPS position.
+    static restaurants = []; // The main table of restaurants.
+    static restaurantPlaceID; // The place_id of a selected restaurant.
+    static actualisationRestaurants; // The active table of restaurants.
+    static markersArray = []; // Table of markers.
 }
